7d55a3ff2fa969d571dbab281318463b4b13775f
unknown <021124@wdhac.com.cn>
2020-09-24 15:22:44 +0800

    2020-09-24

---
+++ fluentpython/16/taxi_sim.py
@@ -0,0 +1,14 @@
+import collections
+
+Event = collections.namedtuple('Event', 'time proc action')
+
+
+def taxi_process(ident, trips, start_time=0):
+    """每次改变状态时创建事件，把控制权让给仿真器"""
+    time = yield Event(start_time, ident, 'leave garage')
+    for i in range(trips):
+        time = yield Event(time, ident, 'pick up passenger')
+        time = yield Event(time, ident, 'drop off passenger')
+
+    yield Event(time, ident, 'going home')
+    # 出租车进程结束
